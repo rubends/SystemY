a10b5679502161ff43c05df866001bd13b05dd67
rubends <rubendeswaef@gmail.com>
2017-11-02 19:45:04 +0100

    added test multicast

---
+++ Multicast/src/be/ua/Main.java
@@ -0,0 +1,10 @@
+package be.ua;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MulticastThread multicastThread = new MulticastThread();
+        multicastThread.start();
+    }
+}

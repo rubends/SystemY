df4f9f9dc903296598aa1a2dae349c29e6ea943d
Stijn Dirickx <stijn.dirickx@hotmail.com>
2017-12-11 11:46:46 +0100

    rmi agent interface added

--- Working_Node/src/be/ua/RMIAgent.java
+++ Working_Node/src/be/ua/RMIAgent.java
@@ -3,18 +3,15 @@ package be.ua;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-
-
-public class RMIAgent extends UnicastRemoteObject {
+public class RMIAgent extends UnicastRemoteObject implements RMIAgentInterface{
     public RMIAgent() throws RemoteException {
     }
 
-    //Maak per node een RMI object waarin een methode staat die:
-    public FileAgent startFileAgent(FileAgent fileAgent) {     //Als parameter een Agent mee krijgt
-        Thread at = new Thread(fileAgent); //Een Thread maakt met de Agent als argument
+    public FileAgent startFileAgent(FileAgent fileAgent) {
+        Thread at = new Thread(fileAgent);
         System.out.println("RMIAGENT.java: File agent started.");
-        at.start(); //De Thread start.
-        while(true) { //Wacht tot de thread stop.
+        at.start();
+        while(true) {
             if (!at.isAlive()) {
 
 
@@ -24,12 +21,17 @@ public class RMIAgent extends UnicastRemoteObject {
         }
     }
 
-    //Dezelfde methode op de volgende node uitvoert (Tenzij de agent moet stoppen, cfr 2.iii)
     public void passFileAgent(FileAgent fileAgent){
         int nextNodeId = 0;
     }
 
 
+    public FileAgent startFailureAgent(FailureAgent failureAgent) throws RemoteException {
+        return null;
+    }
 
 
+    public void passFailureAgent(FailureAgent failureAgent) throws RemoteException {
+
+    }
 }

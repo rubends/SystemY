9c2cae0082572b6a187a27d04c6f0f5173fad756
peetermans1995 <peetermans1995@gmail.com>
2017-12-17 19:55:39 +0100

    Revert "change branch"
    This reverts commit c2d5b2756b0f6aef5c667965758bdbc8864ec89c.

--- Working_Node/src/be/ua/FileAgent.java
+++ Working_Node/src/be/ua/FileAgent.java
@@ -3,13 +3,11 @@ package be.ua;
 import java.io.File;
 import java.io.Serializable;
 import java.util.TreeMap;
-import java.util.Observable;
 
-public class FileAgent extends Observable implements Runnable, Serializable {
+public class FileAgent implements Runnable, Serializable {
 
-    private static TreeMap<String, Boolean> fileList = new TreeMap<>();
-    public FileAgent ()   {
-    }
+    private TreeMap<String, Boolean> fileList = new TreeMap<>();
+
     @Override
     public void run() {
         UserInterface.fileList.clear();
@@ -24,26 +22,12 @@ public class FileAgent extends Observable implements Runnable, Serializable {
         UserInterface.fileList = fileList;
     }
 
-    public static void addToList(File[] files){
+    public void addToList(File[] files){
         for (int i = 0; i < files.length; i++) {
             String filename = files[i].getName();
             if(!fileList.containsKey(filename)){
                 fileList.put(filename, true);
-                System.out.println("adding to file list: " + filename);
             }
         }
-    }
-
-    public static TreeMap<String, Boolean> getFileList()
-    {
-        String rootPath = new File("").getAbsolutePath();
-        String sep = System.getProperty("file.separator"); //OS dependable
-        File localFolder = new File(rootPath + sep + "Files" + sep + "Local");
-        File[] localFiles = localFolder.listFiles();
-        File replicationFolder = new File(rootPath + sep + "Files" + sep + "Replication");
-        File[] replicationFiles = replicationFolder.listFiles();
-        addToList(localFiles);
-        addToList(replicationFiles);
-        return fileList;
     }
 }
